5fa2aa90080dbc11d69e386ef27e4b8e1ff21fe9
linnnnnnng <chinling.yeo@KULGCR-MBOP-142.local>
2013-11-18 15:12:40 +0800

    18 Nov 2013 15:12

--- js/plugins.js
+++ js/plugins.js
@@ -22,33 +22,3 @@
 }());
 
 // Place any jQuery/helper plugins in here.
-
-//ADDRESS HANDLE
-var title = document.title;
-var log = function(msg) {
-	var log = $('.log');
-	if (!log.size()) {
-		//log = $('<div class="log" />').appendTo('.footer');
-	}
-	log.append(msg.replace(/^([^:]*):(.*)$/, '<p><b>$1:</b> <span class="$1">$2</span></p>'))
-		.attr({scrollTop: log.attr('scrollHeight')})
-		.find('p:nth-child(even)').addClass('even');
-};
-var track = function() {
-	log('track: ' + arguments[0]);
-};
-var serialize = function(obj, re) {
-	var result = [];
-	$.each(obj, function(i, val) {
-		if ((re && re.test(i)) || !re)
-			result.push(i + ': ' + (typeof val == 'object' ? val.join 
-				? '\'' + val.join(', ') + '\'' : serialize(val) : '\'' + val + '\''));
-	});
-	return '{' + result.join(', ') + '}';
-};
-
-$.address.init(function(event) {
-    
-}).change(function(event) {
-	log('change: ' + event.pathNames);
-})
